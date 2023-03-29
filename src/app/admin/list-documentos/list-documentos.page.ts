ee84a62cf5f04de8f2a7cbd09402d820e46951ef
Francisco-0510 <francisco109623@gmail.com>
2023-03-29 17:43:10 -0600

    Corrección navegación administrador y encargado

--- src/app/admin/list-documentos/list-documentos.page.ts
+++ src/app/admin/list-documentos/list-documentos.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-documentos',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-documentos.page.scss'],
 })
 export class ListDocumentosPage implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  cerrar() {
+    localStorage.clear();
+    this.router.navigate(['/login']);
   }
-
 }
